a9a5826c9509ada46675435b01ed63d85de65f77
11God <17703283065@163.com>
2018-11-12 15:10:09 +0800

    更改

--- scripts/test_contact.py
+++ scripts/test_contact.py
@@ -8,23 +8,39 @@ from base.base_analyze import analyze_file
 
 class TestContact:
 
-    def setup(self):
-        self.driver = init_driver()
-        self.page = Page(self.driver)
+    def test_add1(self):
+        print('hello1')
+        assert 1
 
-    def teardown(self):
-        time.sleep(5)
-        self.driver.quit()
+    def test_add2(self):
+        print('hello2')
+        assert 1
 
-    @pytest.mark.parametrize("args", analyze_file("contact_data.yml", "test_add_contact"))
-    def test_add_contact(self, args):
-        name = args["name"]
-        phone = args["phone"]
+    def test_add3(self):
+        print('hello3')
+        assert 1
+		
+	def test_add4(self):
+        print('hello4')
+        assert 1
 
-        self.page.contact.click_add_contact()
-        self.page.new_contact.input_name(name)
-        self.page.new_contact.input_phone(phone)
-        self.page.new_contact.click_back()
-        assert name == self.page.saved_contect.get_name_title_text()
+    # def setup(self):
+    #     self.driver = init_driver()
+    #     self.page = Page(self.driver)
+    #
+    # def teardown(self):
+    #     time.sleep(5)
+    #     self.driver.quit()
+    #
+    # @pytest.mark.parametrize("args", analyze_file("contact_data.yml", "test_add_contact"))
+    # def test_add_contact(self, args):
+    #     name = args["name"]
+    #     phone = args["phone"]
+    #
+    #     self.page.contact.click_add_contact()
+    #     self.page.new_contact.input_name(name)
+    #     self.page.new_contact.input_phone(phone)
+    #     self.page.new_contact.click_back()
+    #     assert name == self.page.saved_contect.get_name_title_text()
 
 
